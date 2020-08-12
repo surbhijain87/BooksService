a43d7eb6ea031aecc26c7c8b039b51a112d89d80
surbhijain87 <surbhijain87@gmail.com>
2020-08-12 09:58:47 +0530

    Added notification functionality

--- src/main/java/com/fis/casestudy/books/BooksController.java
+++ src/main/java/com/fis/casestudy/books/BooksController.java
@@ -9,6 +9,7 @@ import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.springframework.transaction.IllegalTransactionStateException;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -36,13 +37,16 @@ public class BooksController {
 
 	@PostMapping("/books/updateAvailability/{bookId}/{incrementCount}")
 	public Book update(@PathVariable("bookId") String bookId, @PathVariable("incrementCount") int incrementCount) {
-
+		System.out.println("Update request for " + bookId);
 		return repository.findById(bookId).map(book -> {
 			int availableCount = book.getAvailable();
 
-			if (availableCount + incrementCount <= book.getTotal()) {
+			if (availableCount + incrementCount <= book.getTotal() && availableCount + incrementCount >= 0) {
 				book.setAvailable(availableCount + incrementCount);
-				checkForNotifications(bookId);
+
+				if (availableCount == 0 && incrementCount > 0) {
+					checkForNotifications(bookId);
+				}
 				return repository.save(book);
 			}
 
@@ -54,22 +58,30 @@ public class BooksController {
 	}
 
 	void checkForNotifications(String bookId) {
+		System.out.println("check for notifications ");
+
 		Properties props = new Properties();
 		props.put("bootstrap.servers", "localhost:9092");
 		props.put("member.id", "ID1234");
 		props.put("group.id", "Group1");
+		props.put("enable.auto.commit", "true");
+		props.put("auto.commit.interval.ms", "1000");
+		props.put("session.timeout.ms", "30000");
+		props.put("max.poll.records", "10");
 		props.put("key.serializer", "org.apache.kafka.common.serialization.StringSerializer");
 		props.put("value.serializer", "org.apache.kafka.common.serialization.StringSerializer");
 		props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
 		props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+
 		try (KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(props)) {
-			consumer.subscribe(Arrays.asList("NotificationTopic"));
+			String topic = "NotificationTopic" + bookId;
+			consumer.subscribe(Arrays.asList(topic));
 
 			ConsumerRecords<String, String> messages = consumer.poll(Duration.ofMillis(1000));
 
 			for (ConsumerRecord<String, String> message : messages) {
-				System.out.printf("Offset = %d, Value = %s\n", message.offset(), message.key(), message.value());
-				// Send email for each
+				System.out.printf("Send book availability notification for Book = %s to User = %s\n", bookId,
+						message.value());
 			}
 		}
 
